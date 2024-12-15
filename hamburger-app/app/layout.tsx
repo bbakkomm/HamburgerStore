77262e96587c0922bc4f94b964350b5f16ba0c36
bbakkomm <bbakkomm@gmail.com>
2024-12-16 06:55:44 +0900

    글로벌 세팅 및 메인 레이아웃 적용

--- hamburger-app/app/layout.tsx
+++ hamburger-app/app/layout.tsx
@@ -1,6 +1,8 @@
+import MainHeader from "@/components/main-header";
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +25,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <MainHeader />
         {children}
       </body>
     </html>
