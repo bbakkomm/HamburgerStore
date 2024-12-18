14ce83f1ae35c980dcd4761ec0e03799881f2426
sunghun <bbakkomm@gmail.com>
2024-12-18 13:51:08 +0900

    nav active 적용

--- hamburger-app/components/main-header.tsx
+++ hamburger-app/components/main-header.tsx
@@ -24,10 +24,10 @@ export default function MainHeader() {
             <nav className={classes.nav}>
                 <ul>
                     <li>
-                        <Link href="/meals" className={path.startsWith('/meals') ? classes.active : ""}>Browse Meals</Link>
+                        <Link href="/meals" className={path.startsWith('/meals') ? classes.active : undefined}>Browse Meals</Link>
                     </li>
                     <li>
-                        <Link href="/community" className={path.startsWith('/community') ? classes.active : ""}>Foodies Community</Link>
+                        <Link href="/community" className={path.startsWith('/community') ? classes.active : undefined}>Foodies Community</Link>
                     </li>
                 </ul>
             </nav>
